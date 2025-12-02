afb6ea42d4a9b5b6b0f5486f2a50d440532d0f4b
darrexx <darx.dev@gmx.de>
2025-12-02 19:51:30 +0100

    Add Day01

---
+++ AdventOfCode2025/Day01/Day01.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2025.Day01;
+
+public class Day01
+{
+    private readonly string[] _inputLines = File.ReadAllLines("G:\\Dev\\AdventOfCode2025\\AdventOfCode2025\\input.txt");
+
+    public int Part1()
+    {
+        var start = 50;
+        var count = 0;
+
+        foreach (var line in _inputLines)
+        {
+            var direction = line[0];
+            if (direction == 'R')
+            {
+                start += int.Parse(line[1..]);
+            }
+            else
+            {
+                start -= int.Parse(line[1..]);
+            }
+            if (start % 100 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public int Part2()
+    {
+        var start = 50;
+        var count = 0;
+
+        foreach (var line in _inputLines)
+        {
+            var direction = line[0];
+            var changeBy = direction == 'R' ? 1 : -1;
+            for(var i = 0; i < int.Parse(line[1..]); i++)
+            {
+                start += changeBy;
+                if (start % 100 == 0)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ AdventOfCode2025/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2025.Day01;
+
+var day01 = new Day01();
+Console.WriteLine(day01.Part1());
+Console.WriteLine(day01.Part2());
