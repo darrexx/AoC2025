a5dfc820f79837615e84f98de6490fc671b65b51
darrexx <darx.dev@gmx.de>
2025-12-02 20:40:00 +0100

    Add Day02 Part1

---
+++ AdventOfCode2025/Day02/Day02.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode2025.Day02;
+
+public class Day02
+{
+    private readonly string _input;
+
+    public Day02()
+    {
+        _input = File.ReadAllText("");
+    }
+    
+    public long Part1()
+    {
+        var ranges = _input.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Split("-"))
+            .Select(x => (x[0], x[1]));
+
+        var sum = 0L;
+        foreach (var range in ranges)
+        {
+            var elements = EnumerableRangeLong(long.Parse(range.Item1), long.Parse(range.Item2) - long.Parse(range.Item1) + 1)
+                .Select(x => x.ToString());
+            
+            var grouped = elements.Where(x => x.Length % 2 == 0)
+                .Select(x => new { HalfLength = x.Length / 2, Number = x });
+            sum += grouped.Where(x => x.Number[..x.HalfLength] == x.Number[x.HalfLength..]).Select(x => long.Parse(x.Number)).Sum();
+            
+            
+        }
+        return sum;
+    }
+
+    private static IEnumerable<long> EnumerableRangeLong(long start, long count)
+    {
+        var end = start + count;
+
+        while (start < end)
+        {
+            yield return start;
+            start++;
+        }
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AdventOfCode2025.Day01;
+using AdventOfCode2025.Day02;
 
-var day01 = new Day01();
-Console.WriteLine(day01.Part1());
-Console.WriteLine(day01.Part2());
+// var day01 = new Day01();
+// Console.WriteLine(day01.Part1());
+// Console.WriteLine(day01.Part2());
+
+var day02 = new Day02();
+Console.WriteLine(day02.Part1());
