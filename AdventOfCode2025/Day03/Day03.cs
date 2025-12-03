5f960955ce4fb598d21ca278ec139389fcb304a1
darrexx <darx.dev@gmx.de>
2025-12-03 22:50:11 +0100

    Add Day03 Part1

---
+++ AdventOfCode2025/Day03/Day03.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2025.Day03;
+
+public class Day03
+{
+    private readonly string[] _input;
+
+    public Day03()
+    {
+        _input = File.ReadAllLines("");
+    }
+
+    public long Part1()
+    {
+        var batterieRows = _input.Select(x => x.Select((y, index) => ((int)char.GetNumericValue(y), index)).ToArray()).ToArray();
+        var sum = 0;
+        foreach (var batteryRow in batterieRows)
+        {
+            var largestFirst = batteryRow[..^1].MaxBy(x => x.Item1);
+            var largestSecond = batteryRow[(largestFirst.index+1)..].MaxBy(x => x.Item1);
+            sum += int.Parse(largestFirst.Item1.ToString() + largestSecond.Item1.ToString());
+        }
+        
+        return sum;
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -2,11 +2,15 @@
 
 using AdventOfCode2025.Day01;
 using AdventOfCode2025.Day02;
+using AdventOfCode2025.Day03;
 
 // var day01 = new Day01();
 // Console.WriteLine(day01.Part1());
 // Console.WriteLine(day01.Part2());
 
-var day02 = new Day02();
-Console.WriteLine(day02.Part1());
-Console.WriteLine(day02.Part2());
+// var day02 = new Day02();
+// Console.WriteLine(day02.Part1());
+// Console.WriteLine(day02.Part2());
+
+var day03 = new Day03();
+Console.WriteLine(day03.Part1());
